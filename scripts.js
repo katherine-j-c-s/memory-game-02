5473b9340a45eb35a4eb1f1ec0b2f4f5196205f0
katherine Contreras <kathijcs@gmail.com>
2021-02-08 21:20:44 -0300

    add back-face's style, rotate, perspective and perserve-3d to flip card

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
 (function () {
-    const cards = document.querySelectorAll('.memory-card');
+    const cards = document.querySelectorAll('.memory-card'); // es un areray de elementos que tienen l;a clase memory card
     console.log(cards);
     function flipCard() {
         console.log("clik",this);
-        this.classList.toggle('flip');
+        this.classList.toggle('flip'); // -> si existe la clase "flip" la va a eliminar, si no existe, la va a agregar
     }
-    for (var i= 0; i<cards.length; i++){
+    for (var i=0; i<cards.length; i++){
+        console.log(`%c index: ${i} elemento`,'color:green',cards[i])
         cards[i].addEventListener('click', flipCard);
     }
 }());
