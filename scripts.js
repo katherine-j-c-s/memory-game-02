99059a85d5601dd4560e8400598b7d6b97ab8ad6
katherine Contreras <kathijcs@gmail.com>
2021-02-22 21:08:20 -0300

    added data-framework to each memory-card and a if to know if a card has flipped

--- scripts.js
+++ scripts.js
@@ -1,12 +1,21 @@
 (function () {
     const cards = document.querySelectorAll('.memory-card'); // es un areray de elementos que tienen l;a clase memory card
-    console.log(cards);
+ //   console.log(cards);
+    var hasFlippedCard = false;
+    var firstCard, secondCard;
     function flipCard() {
         console.log("clik",this);
-        this.classList.toggle('flip'); // -> si existe la clase "flip" la va a eliminar, si no existe, la va a agregar
+        this.classList.add('flip'); // -> si existe la clase "flip" la va a eliminar, si no existe, la va a agregar
+        console.log("Esta volteada la carta?",hasFlippedCard)
+        if(!hasFlippedCard) {
+            hasFlippedCard = true;
+            firstCard = this;
+        }
+        console.log("bandera",hasFlippedCard)
+        console.log("first card",firstCard)
     }
     for (var i=0; i<cards.length; i++){
-        console.log(`%c index: ${i} elemento`,'color:green',cards[i])
+     //   console.log(`%c index: ${i} elemento`,'color:green',cards[i])
         cards[i].addEventListener('click', flipCard);
     }
 }());
