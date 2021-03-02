ee909bc2e40ce7db1802c7b592e76a1434f8d384
katherine Contreras <kathijcs@gmail.com>
2021-03-01 21:48:41 -0300

    blocked the board while two card has flipped, dont flip a card if its the same one and added a function resetBoard

--- scripts.js
+++ scripts.js
@@ -2,9 +2,15 @@
     const cards = document.querySelectorAll('.memory-card'); // es un areray de elementos que tienen l;a clase memory card
  //   console.log(cards);
     var hasFlippedCard = false;
+    var lockBoard = false;
     var firstCard, secondCard;
     function flipCard() {
         console.log("clik",this);
+        if (lockBoard) {
+            return;
+        } 
+        // si la segunda carta es igual a la primera (es la misma carta)
+        if (this === firstCard) return; // -> vamos a asumir que esto esta bien, luego investigamos por que
         this.classList.add('flip'); // -> si existe la clase "flip" la va a eliminar, si no existe, la va a agregar
         console.log("habia una carda volteada?",hasFlippedCard)
         if(!hasFlippedCard) {
@@ -14,7 +20,6 @@
             return;
         }
         secondCard = this;
-        hasFlippedCard = false;
         console.log("Esta volteada la carta?",hasFlippedCard)
         console.log("first card",firstCard)
         console.log("second card",secondCard)
@@ -33,14 +38,28 @@
     function disableCards(){
         firstCard.removeEventListener('click',flipCard);
         secondCard.removeEventListener('click',flipCard);
+        resetBoard();
     }
     function unflipCards() {
-        setTimeout(removeFlipClass,1500)
+        lockBoard = true;
+        // en js puedes tener funciones que tomen como parametro otras funciones
+        setTimeout(removeFlipClass,500); 
+    }
+    function resetBoard() {
+        lockBoard = false;
+        hasFlippedCard = false;
+        firstCard = null;
+        secondCard = null;
+        /* ES6 destructuring
+        [lockBoard, hasFlippedCard] = [false,false];
+        [firstCard, secondCard] = [null, null];
+        */
     }
     function removeFlipClass(){
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
         console.log("removeFlipClass")
+        resetBoard();
     }
     for (var i=0; i<cards.length; i++){
      //   console.log(`%c index: ${i} elemento`,'color:green',cards[i])
